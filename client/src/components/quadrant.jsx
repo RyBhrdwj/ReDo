30150dbcfa5e6e26ef3d7446288cbc4ed35af88a
RyBhrdwj <25ricky08@gmail.com>
2024-04-18 18:38:54 +0530

    refactor code

--- client/src/components/quadrant.jsx
+++ client/src/components/quadrant.jsx
@@ -8,12 +8,16 @@ export default function Quadrant() {
     {title: "URGENT", color: "bg-blue-500"},
     {title: "NONE", color: "bg-green-500"}
   ]
+
+  const renderSections = (data) => {
+    return data.map((section, index) => {
+      return <Section key={index} title={section.title} color={section.color}/>
+    })
+  }
     
   return (
     <div className="overflow-hidden box-border h-full w-full max-w-[2000px] m-auto bg-stone-900 grid grid-cols-2 grid-rows-2 gap-2 p-2">
-      {section_data.map((section, index) => (
-        <Section key={index} title={section.title} color={section.color} />
-      ))}
+      {renderSections(section_data)}
     </div>
   );
   
--- client/src/components/section.jsx
+++ client/src/components/section.jsx
@@ -2,7 +2,7 @@ export default function Section({title, color}) {
 
   return (
     <div className = {`border-1 border-white rounded-lg ${color} p-2`}>
-    <h1 className=" text-5xl text-center font-bold text-stone-900">
+    <h1 className=" text-4xl text-center font-bold text-stone-900">
       {title}
     </h1>
     </div>
