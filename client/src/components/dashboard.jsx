40f921e9a98d9b8439c060ef8a8e552ca92c8323
RyBhrdwj <25ricky08@gmail.com>
2024-04-19 15:44:47 +0530

    add drawer

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -1,10 +1,10 @@
+import MiniDrawer from "./drawer";
 import Quadrant from "./quadrant";
-import Section from "./section";
 
 export default function Dashboard() {
   return (
     <div className="flex h-full">
-      <div className=" w-1/5 bg-stone-900"> NAVBAR</div>
+      <MiniDrawer />
       <Quadrant />
     </div>
   );
