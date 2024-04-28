86ea26f8240ad0445e76f29c0e21bfa25b698e42
RyBhrdwj <25ricky08@gmail.com>
2024-04-28 19:43:50 +0530

    make create task section a form and add onKeyDown event to add task

--- client/src/components/CreateTaskCard.jsx
+++ client/src/components/CreateTaskCard.jsx
@@ -18,11 +18,9 @@ export default function CreateTaskCard({ section }) {
   // };
 
   const handleAddTask = (e) => {
-
     e.stopPropagation();
 
     if (!taskName.trim()) {
-
       alert("Please enter a task name");
       return;
     }
@@ -37,37 +35,40 @@ export default function CreateTaskCard({ section }) {
       },
     });
 
-    dispatch({ type: "SET_SHOW_CREATE_TASK", payload: 0 })
+    dispatch({ type: "SET_SHOW_CREATE_TASK", payload: 0 });
 
     setTaskName("");
     // setDateTime(new Date());
   };
 
   return (
-      <div
-        className=" relative m-2 rounded-lg bg-white/10 p-2"
-        onClick={(e) => e.stopPropagation()}
+    <form
+      className="relative m-2 rounded-lg bg-white/10 p-2"
+      onClick={(e) => e.stopPropagation()}
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleAddTask(e);
+      }}
+    >
+      <input
+        autoFocus
+        type="text"
+        placeholder="Enter task name"
+        value={taskName}
+        onChange={handleTaskNameChange}
+        className="w-full rounded-lg bg-white/20 p-2 text-black placeholder-white/50 transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-white/80 focus:ring-opacity-50"
+      />
+      {/* <DatetimePicker
+              onChange={handleDateTimeChange}
+              value={dateTime}
+          /> */}
+      <button
+        type="submit"
+        className="duration-2 right-16 mt-3 rounded-lg bg-white/60 p-2 font-semibold text-black outline outline-2 outline-white/70 transition-all ease-in-out hover:rounded-3xl  hover:bg-green-500 hover:text-white"
       >
-        <input
-          autoFocus
-          type="text"
-          placeholder="Enter task name"
-          value={taskName}
-          onChange={handleTaskNameChange}
-          className="w-full rounded-lg bg-white/20 p-2 text-black placeholder-white/50 transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-white/80 focus:ring-opacity-50"
-        />
-        {/* <DatetimePicker
-                onChange={handleDateTimeChange}
-                value={dateTime}
-            /> */}
-        <button
-          className="duration-2 right-16 mt-3 rounded-lg bg-white/60 p-2 font-semibold text-black outline outline-2 outline-white/70 transition-all ease-in-out hover:rounded-3xl  hover:bg-green-500 hover:text-white"
-          onClick={(e) => {
-            handleAddTask(e);
-          }}
-        >
-          Add Task
-        </button>
-      </div>
+        Add Task
+      </button>
+    </form>
   );
+  
 }
