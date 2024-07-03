e0f779b8838de66034f13a4c974a7564fba4d23f
RyBhrdwj <25ricky08@gmail.com>
2024-07-04 01:47:12 +0530

    update task model

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   name: {
     type: String,
     required: true,
